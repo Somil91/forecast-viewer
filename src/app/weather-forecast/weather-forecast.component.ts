139f349f0d782b545649acb09a0dd9c6eb86f737
Somil Kumar <somil.kumar91@gmail.com>
2019-02-26 11:30:40 +0100

    Adding changes for the images

--- src/app/weather-forecast/weather-forecast.component.ts
+++ src/app/weather-forecast/weather-forecast.component.ts
@@ -18,25 +18,25 @@ export class WeatherForecastComponent implements OnInit {
     // {
     //   id: 3067696,
     //   name: 'Prague',
-    //   imageUrl: 'http://placeholder.it/240x240',
+    //   imageUrl: 'https://unsplash.com/photos/oUz3TepCSko',
     //   country: 'Czech Republic'
     // },
     // {
     //   id: 2988507,
     //   name: 'Paris',
-    //   imageUrl: 'http://placeholder.it/240x240',
+    //   imageUrl: 'https://unsplash.com/photos/vowZFVPFzK0',
     //   country: 'France'
     // },
     // {
     //   id: 2761369,
     //   name: 'Vienna',
-    //   imageUrl: 'http://placeholder.it/240x240',
+    //   imageUrl: 'https://unsplash.com/photos/5SjAaqqCCmY',
     //   country: 'Austria'
     // },
     // {
     //   id: 3067696,
     //   name: 'Berlin',
-    //   imageUrl: 'http://placeholder.it/240x240',
+    //   imageUrl: 'https://unsplash.com/photos/iPOZf3tQfHA',
     //   country: 'Germany'
     // },
   ];
